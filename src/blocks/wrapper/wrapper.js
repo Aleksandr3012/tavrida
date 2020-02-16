132ef5972208c205e3742e043c5778f8e83f95c5
re-mote <headof.remote@gmail.com>
2020-02-16 17:23:50 +0300

    block about css & feedback css & wrapper css js

--- src/blocks/wrapper/wrapper.js
+++ src/blocks/wrapper/wrapper.js
@@ -2,5 +2,8 @@ if ($(window).width() > 992) {
   new fullpage('#fullpage', {
     navigation: true,
     normalScrollElements: '.content__festival .content__forum',
+    onLeave: function (origin, destination, direction) {
+      $('#fp-nav').attr('data-slide', destination.index);
+    }
   });
 }
