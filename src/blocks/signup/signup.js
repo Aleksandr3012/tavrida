4427a72ce445a6c519c5402459a6a992b88cb88c
re-mote <headof.remote@gmail.com>
2020-02-24 17:52:08 +0100

    Доверстал десктоп основной формы регистрации

--- src/blocks/signup/signup.js
+++ src/blocks/signup/signup.js
@@ -2,3 +2,8 @@ $('.js-registration').click(function(e) {
   e.preventDefault();
   $('.signup').fadeIn('fast');
 });
+
+$('.signup__close').click(function(e) {
+  e.preventDefault();
+  $('.signup').fadeOut('fast');
+});
