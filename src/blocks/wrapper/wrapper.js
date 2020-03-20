7689cde0f629e727c4b544dcec5cf4dfa2bd4c8c
re-mote <headof.remote@gmail.com>
2020-03-20 11:41:54 +0300

    Поправил видео и логотип

--- src/blocks/wrapper/wrapper.js
+++ src/blocks/wrapper/wrapper.js
@@ -1,10 +1,12 @@
 if ($(window).width() > 992) {
   new fullpage('#fullpage', {
     navigation: true,
-//    normalScrollElements: '.highlights',
     fixedElements: '.signup',
     onLeave: function (origin, destination, direction) {
       $('#fp-nav').attr('data-slide', destination.index);
+      if(destination.index == '0') {
+        $('.front__bg').get(0).play();
+      }
     }
   });
 }
