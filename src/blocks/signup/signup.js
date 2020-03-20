c4e91ce1d20339d1443dca20aaab3e2c6c9bab92
re-mote <headof.remote@gmail.com>
2020-03-20 17:26:06 +0300

    Поправил стили для селекта, поправил скрипт в соответствии с продакшном

--- src/blocks/signup/signup.js
+++ src/blocks/signup/signup.js
@@ -20,10 +20,10 @@ $('.signup__close').click(function (e) {
 });
 
 $(document).click(function (e) {
-  if ($(e.target).closest('.signup').length === 0 && $(e.target).closest('.js-registration').length === 0 && $(e.target).closest('.js-login').length === 0) {
-    $('.signup').fadeOut('fast');
-    $('.wrapper').removeClass('wrapper--popup');
-  $('body').removeClass('body--popup');
+  if ($(e.target).closest('.signup').length === 0 && $(e.target).closest('.container-item-remove').length === 0 && $(e.target).closest('.js-registration').length === 0 && $(e.target).closest('.js-login').length === 0) {
+    +$('.signup').fadeOut('fast'); +
+    $('.wrapper').removeClass('wrapper--popup'); +
+    $('body').removeClass('body--popup');
   }
 });
 
