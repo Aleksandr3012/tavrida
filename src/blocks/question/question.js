11adcb39e5c2e652b57351c26ca9633f2a36199d
re-mote <headof.remote@gmail.com>
2020-04-01 00:32:15 +0300

    Сверстал десктоп блока faq

---
+++ src/blocks/question/question.js
@@ -0,0 +1,5 @@
+$('.question__header').click(function(e) {
+  e.preventDefault();
+  $(this).closest('.question').toggleClass('question--active');
+  $(this).closest('.question').find('.question__content').slideToggle('fast');
+});
