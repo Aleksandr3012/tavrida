fdf0dca14baa98e37140c05c166bf8625e65db21
re-mote <headof.remote@gmail.com>
2020-04-01 14:50:49 +0300

    Доверстал страницу грантов

--- src/blocks/item-slider/item-slider.js
+++ src/blocks/item-slider/item-slider.js
@@ -1,4 +1,19 @@
 $('.item-slider').slick({
   slidesToShow: 3,
-  infinite: false
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        autoplay: true
+      }
+    },
+  ]
 })
