67f0d468c272a02c690d1f2fc9c8d249f8790c53
re-mote <headof.remote@gmail.com>
2020-04-03 11:44:06 +0300

    Отключил скролл в блоке .highlights

--- src/blocks/highlights/highlights.js
+++
@@ -1,7 +0,0 @@
-if ($(window).width() > 992 && $('.highlights__content').length) {
-  const hps = new PerfectScrollbar('.highlights__content', {
-    axis: 'y',
-    wheelSpeed: 0.5,
-    wheelPropagation: true
-  });
-}
