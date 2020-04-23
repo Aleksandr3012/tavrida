d01b73b98587912a0da49fc557758c645580b404
re-mote <headof.remote@gmail.com>
2020-04-23 14:12:23 +0300

    Поправил выравнивание заголовков в блоке .steps

---
+++ src/blocks/step/step.js
@@ -0,0 +1,31 @@
+//var heights = $(".step__header").map(function () {
+//  return $(this).height();
+//}).get();
+//
+//maxHeight = Math.max.apply(null, heights);
+//console.log(maxHeight);
+//$('.step__header').css('height', maxHeight);
+
+//$('.team__steps').each(function() {
+//  var heights = $(this).find('.step__header').map(function () {
+//    return $(this).height();
+//  }).get();
+//  maxHeight = Math.max.apply(null, heights);
+//  console.log(maxHeight);
+//  $(this).find('.step__header').css('height', maxHeight);
+//});
+
+
+$(window).on('load resize', function() {
+  if ($(window).width() > 767) {
+    $('.step__header').css('height', 'auto');
+    $('.team__steps').each(function() {
+      var heights = $(this).find('.step__header').map(function () {
+        return $(this).height();
+      }).get();
+      maxHeight = Math.max.apply(null, heights);
+      console.log(maxHeight);
+      $(this).find('.step__header').css('height', maxHeight);
+    });
+  }
+})
