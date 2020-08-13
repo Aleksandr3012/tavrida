e6c32c4a0ad91e2a72226218e86fbb1fecf3ee7a
alpha <alpha@re-mote.ru>
2020-08-13 13:58:31 +0300

    update

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -3,3 +3,16 @@ $('.input--phone').inputmask("+7 (999) 999-99-99");
 $('.input--passport-series').inputmask("99 99");
 
 $('.input--code').inputmask("999-999");
+
+$('.input--date').datepicker();
+
+$('.input--time').timepicker({
+    interval: 60,
+    minTime: '10',
+    maxTime: '6:00pm',
+    defaultTime: '11',
+    startTime: '10:00',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+});
