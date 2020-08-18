76fb75a0306e8146b1f11a42fbdc32286893c66b
alpha <alpha@re-mote.ru>
2020-08-18 16:25:10 +0300

    Сверстал слайдеры кастингов

---
+++ src/blocks/castings-slider/castings-slider.js
@@ -0,0 +1,27 @@
+if ($('.castings-slider__content').length) {
+
+  $('.castings-slider__content').slick({
+    slidesToShow: 4,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+}
