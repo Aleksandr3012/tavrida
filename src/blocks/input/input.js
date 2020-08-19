f1f0fbfaca393c40f852a65021f7989fba0ac895
alpha <alpha@re-mote.ru>
2020-08-19 19:14:56 +0300

    Откат vendor.js

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -4,15 +4,15 @@ $('.input--passport-series').inputmask("99 99");
 
 $('.input--code').inputmask("999-999");
 
-$('.input--date').datepicker();
-
-$('.input--time').timepicker({
-    interval: 60,
-    minTime: '10',
-    maxTime: '6:00pm',
-    defaultTime: '11',
-    startTime: '10:00',
-    dynamic: false,
-    dropdown: true,
-    scrollbar: true
-});
+//$('.input--date').datepicker();
+//
+//$('.input--time').timepicker({
+//    interval: 60,
+//    minTime: '10',
+//    maxTime: '6:00pm',
+//    defaultTime: '11',
+//    startTime: '10:00',
+//    dynamic: false,
+//    dropdown: true,
+//    scrollbar: true
+//});
