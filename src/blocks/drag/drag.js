300922d7a228283a2d4bd5c191efa9e6bf2a3357
Natusik <test@re-mote.ru>
2020-10-01 07:48:07 +0300

    fix drag

--- src/blocks/drag/drag.js
+++ src/blocks/drag/drag.js
@@ -1,8 +1,22 @@
 if ($(window).width() > 1199) {
 
-  const slider3 = new ScrollBooster({
-    viewport: document.querySelector('.drag__content'),
-    content: document.querySelector('.drag'),
+  const slider4 = new ScrollBooster({
+    viewport: document.querySelector('.js-drag-video'),
+    content: document.querySelector('.js-drag-video-content'),
+    scrollMode: 'transform',
+    direction: 'horizontal',
+  });
+
+  const slider5 = new ScrollBooster({
+    viewport: document.querySelector('.js-drag-artist1'),
+    content: document.querySelector('.js-drag-artist1-content'),
+    scrollMode: 'transform',
+    direction: 'horizontal',
+  });
+
+  const slider6 = new ScrollBooster({
+    viewport: document.querySelector('.js-drag-artist2'),
+    content: document.querySelector('.js-drag-artist2-content'),
     scrollMode: 'transform',
     direction: 'horizontal',
   });
