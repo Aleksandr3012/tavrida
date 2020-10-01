a2524d267fab9ff292bf95e2d8e8add7b10afdb2
Natusik <test@re-mote.ru>
2020-10-01 07:25:48 +0300

    fest-new адаптивность

--- src/blocks/drag/drag.js
+++ src/blocks/drag/drag.js
@@ -1,6 +1,9 @@
-const slider3 = new ScrollBooster({
-  viewport: document.querySelector('.drag__content'),
-  content: document.querySelector('.drag'),
-  scrollMode: 'transform',
-  direction: 'horizontal',
-});
+if ($(window).width() > 1199) {
+
+  const slider3 = new ScrollBooster({
+    viewport: document.querySelector('.drag__content'),
+    content: document.querySelector('.drag'),
+    scrollMode: 'transform',
+    direction: 'horizontal',
+  });
+}
