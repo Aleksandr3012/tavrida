f2dff691157852240e3d674bab37ef3fcec9d61d
Natusik <test@re-mote.ru>
2020-10-01 06:35:56 +0300

    update festival-new

---
+++ src/blocks/drag/drag.js
@@ -0,0 +1,6 @@
+const slider3 = new ScrollBooster({
+  viewport: document.querySelector('.drag__content'),
+  content: document.querySelector('.drag'),
+  scrollMode: 'transform',
+  direction: 'horizontal',
+});
